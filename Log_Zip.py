65aeb753c5bf61fb0ed7b416948b373df0d16e86
Francis PC <francis.rouillon.openclassrooms@gmail.com>
2020-10-11 15:58:58 +0200

    Ajout du test et de la creation du repertoire cible s'il n'existe pas modification du fichier config.ini pour les executions suivantes.

--- Log_Zip.py
+++ Log_Zip.py
@@ -8,10 +8,9 @@ dic = {}
 
 # Paramêtrage des répertoires de travail avec un fichier config.ini
 print()
-print("Utilisation du fichier " + local + "\config.ini")
+
 
 fichier_conf = local + "/config.ini"
-
 
 
 with open(fichier_conf,"r") as conf:
@@ -24,10 +23,16 @@ print()
 
 path_in = str((dic["path_in"]).replace("\\","/"))
 path_out = str((dic["path_out"]).replace("\\","/"))
+
 doctype = dic["doctype"]
 config = dic["config"]
 taille = dic["taille"]
-print(config)
+
+if not os.path.exists(path_in):
+    print("le répertoire source n'existe pas ")
+    quit()
+
+
 
 if  os.path.exists(path_out):
     direxists="oui"
@@ -38,29 +43,48 @@ else:
 
 
 if direxists == "oui" and config == "oui":
-    print("utilisation du dossier d'archives : ".format(path_out))
+    print("utilisation du dossier d'archives : {} ".format(path_out).replace("/","\\"))
 
 elif direxists == "oui" and config == "non":
 
     est_sur = True
     while est_sur:
-                sur = input("Le dossier {} existe déjà, \nêtes vous sur de vouloir utiliser ce dossier ? ".format(path_out))
+                sur = input("Le dossier {} existe déjà, \nêtes vous sur de vouloir utiliser ce dossier ? ".format(path_out).replace("/","\\"))
                 sur = sur.lower()
                 if sur == "n":
-                    print("on quite")
+                    print("on quitte")
                     quit()
                 elif sur == "o":
                     est_sur = False
                 else:
-                    print(chr(27)+'[2j')
                     print("Quitter : n , accepter : o")
 
 
-
-
 elif direxists == "non":
-    creation_dossier = input("le dossier {} n'existe pas, souhaitez vous le créer ? ".format(path_out))
-    print("creation du dossier d'achives : {}".format(path_out))
+    est_sur = True
+    while est_sur:
+        creation_dossier = input("le dossier {} n'existe pas, souhaitez vous le créer ? ".format(path_out))
+        creation_dossier = creation_dossier.lower()
+        if creation_dossier == "n":
+            print("Programme arrêté par l'utilisateur")
+            quit()
+        elif creation_dossier == "o":
+            est_sur = False
+            print("creation du dossier : {}".format(path_out).replace("/","\\"))
+            os.mkdir(path_out.replace("/","\\"))
+            if config == "non":
+                fic_conf = open(fichier_conf,"rt")
+                contenu = fic_conf.read()
+                contenu = contenu.replace("non","oui")
+                fic_conf.close()
+                fic_conf = open(fichier_conf,"wt")
+                fic_conf.write(contenu)
+                fic_conf.close()
+        else:
+            print("Quitter : n , accepter : o")
+
+
+
 
 
 
